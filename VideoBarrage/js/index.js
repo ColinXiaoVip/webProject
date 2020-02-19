8326f3fff576fdce250c3f6ee4a3a04309f01c41
ColinXiao <2513651411@qq.com>
2020-02-19 17:48:51 +0800

    jq color

---
+++ VideoBarrage/js/index.js
@@ -0,0 +1,23 @@
+$(() => {
+    let video_width = $('#video').css('width');
+    console.log(video_width)
+    $('#barrage_btn').click(() => {
+        let val_input = $('#barrage_input').val();
+        let val = '<p>' + val_input + '</p>';
+        $('#video').append(val)
+        $('#barrage_input').val('');
+    });
+
+    setInterval(() => {
+        function randomColor() {
+            let color = "";
+            for (let i = 0; i < 6; i++) {
+                color += (Math.random() * 16 | 0).toString(16);
+            }
+            return "#" + color;
+        }
+
+        let color = randomColor();
+        $('p').css('color', color)
+    }, 1000)
+});
